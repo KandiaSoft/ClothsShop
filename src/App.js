9107c528edeb72d9011c3cb483635e3d9a8112de
Carlos Morante <cmorante@tsomobile.com>
2020-05-10 15:31:44 -0500

    seccion 6-72

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from './pages/homepage/homepage.component'
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 const HatsPage = () => (
   <div>
-    <h1>HATS PAGE </h1>
+    <h1>HATS PAGE</h1>
   </div>
 );
 
@@ -15,7 +16,7 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
