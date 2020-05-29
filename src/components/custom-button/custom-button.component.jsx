5fc6775fc5b452b9a7146eb90e92a7c7c072f578
Carlos Morante <cmorante@tsomobile.com>
2020-05-28 21:37:29 -0500

    Section 8-116

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomButton = ({children, isGoogleSignIn, ...other}) => (
-    <button className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...other}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...other}) => (
+    <button className={
+        `${inverted ? 'inverted' : ''} 
+         ${isGoogleSignIn ? 'google-sign-in' : ''} 
+         custom-button`} {...other}>
         {children}
     </button>
 );
