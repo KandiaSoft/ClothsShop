13102ec9542d0ded8599b9172b941d47569757e1
Carlos Morante <cmorante@tsomobile.com>
2020-06-07 13:50:22 -0500

    Section 13-146

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51GrSe1EneHEFggkWVkfbSi4kLMDjaEqJTRgMVopvGsk1q40z1ovwhOC4pBnf6fMVZ1kV4AAibbwU9d50XLfqdS3f00ly48a6kh';
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Successful');
+    }
+
+    return (
+        <StripeCheckout
+            label = 'Pay Now'
+            name = 'CRWN Clothing Ltd.'
+            billingAddress
+            shippingAddress
+            image = 'https://svgshare.com/i/Cuz.svg'
+            description = {`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel= 'Pay Now'
+            token={onToken}
+            stripeKey ={publishableKey}
+        />
+
+  
+    );
+};
+
+export default StripeCheckoutButton;
